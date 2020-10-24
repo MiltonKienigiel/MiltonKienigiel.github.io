6a1799c9302e2a2e0ef54e43951c957630d66dbc
MiltonKienigiel <miltonkienigiel@gmail.com>
2020-10-23 21:14:40 -0300

    update chau insta
    falta arreglar el spanish

---
+++ js/index.js
@@ -0,0 +1,10 @@
+let spanish = document.getElementById("english");
+let descripcion = document.getElementById("gris");
+let titulo2 = document.getElementById("titulo2");
+let titulo4 = document.getElementById("titulo4");
+
+spanish.onclick=()=>{
+    descripcion.innerHTML="Desarrollador Web";
+    titulo2.innerHTML="Herramientas";
+    titulo4.innerHTML = "En Proceso de aprendizaje"
+}
