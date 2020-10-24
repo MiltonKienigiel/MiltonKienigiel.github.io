f0d64ff726e8c5c8dfcd89690aaa310a288fc27f
MiltonKienigiel <miltonkienigiel@gmail.com>
2020-10-23 22:40:06 -0300

    update css

--- js/index.js
+++ js/index.js
@@ -1,10 +1,38 @@
 let spanish = document.getElementById("english");
 let descripcion = document.getElementById("gris");
-let titulo2 = document.getElementById("titulo2");
+let titulo2 = document.getElementById("herramienta");
 let titulo4 = document.getElementById("titulo4");
+let titulo3 = document.getElementById("proyecto");
+let titulo5 = document.getElementById("titulo5");
+let titulo6 = document.getElementById("contactos");
+let header1 = document.getElementById("about");
+let header2 = document.getElementById("proyectos");
+let header3 = document.getElementById("contacto");
+let header4 = document.getElementById("english");
 
-spanish.onclick=()=>{
-    descripcion.innerHTML="Desarrollador Web";
-    titulo2.innerHTML="Herramientas";
-    titulo4.innerHTML = "En Proceso de aprendizaje"
+if (spanish.innerHTML=="Español") {
+    spanish.onclick=()=>{
+        spanish.innerHTML= "English";
+        descripcion.innerHTML= "Desarrollador Web";
+        titulo4.innerHTML = "En Proceso de aprendizaje"
+        titulo2.innerHTML= "Habilidades";
+        titulo3.innerHTML= "Proyectos";
+        titulo5.innerHTML= "Plataforma de Musica"
+        titulo6.innerHTML= "Contacto"
+        header1.innerHTML= "Acerca de Mí"
+        header2.innerHTML= "Habilidades"
+        header3.innerHTML= "Proyectos"
+        header4.innerHTML= "Contáctame"
+
+    }
+} else{
+    spanish.onclick=()=>{
+        spanish.innerHTML="Spanish";
+        descripcion.innerHTML="Web Developer";
+        titulo4.innerHTML = "In Process"
+        titulo2.innerHTML="Mastered Skills";
+        titulo3.innerHTML="Projects";
+        titulo5.innerHTML="Music Platform"
+    
+    }
 }
